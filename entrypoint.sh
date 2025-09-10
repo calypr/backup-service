96e3d1a6d18701024bd22a757d51ccff364a20f5
Liam Beckman <lbeckman314@gmail.com>
2025-09-09 17:56:08 -0700

    feat: update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,29 +1,18 @@
 #!/bin/bash
 set -e
 
-echo "Backup started at $(date)"
+TIMESTAMP=$(date +"%Y-%m-%dT%H:%M:%S")
+
+DIR="${DIR/$TIMESTAMP}"
 
 # Postgres Dump
-echo "Running Postgres dump..."
 bak --debug pg dump \
     --dir "${DIR}" \
     --host "${PGHOST}" \
     --user "${PGUSER}" \
     --password "${PGPASSWORD}"
 
-# S3 Upload
-echo "Running S3 upload..."
-bak --debug s3 upload \
-    --dir "${DIR}" \
-    --endpoint "${ENDPOINT}" \
-    --bucket "${BUCKET}" \
-    --key "${KEY}" \
-    --secret "${SECRET}"
-
-echo "Postgres Backup completed at $(date)"
-
 # GRIP Backup
-echo "Running GRIP backup..."
 bak --debug grip backup \
     --dir "${DIR}" \
     --host "${GRIP_HOST}" \
@@ -32,12 +21,9 @@ bak --debug grip backup \
     --edge
 
 # S3 Upload
-echo "Running S3 upload..."
 bak --debug s3 upload \
     --dir "${DIR}" \
     --endpoint "${ENDPOINT}" \
     --bucket "${BUCKET}" \
     --key "${KEY}" \
---secret "${SECRET}"
-
-echo "GRIP Backup completed at $(date)"
+    --secret "${SECRET}"
