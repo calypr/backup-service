b5930e4f537a91aa2fbf9e8d7c893be3df55fc85
Liam Beckman <lbeckman314@gmail.com>
2026-01-05 13:25:38 -0800

    feat: Add additional output to entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -30,12 +30,14 @@ TIMESTAMP=$(date +"%Y-%m-%dT%H:%M:%S")
 export DIR="${DIR}/${TIMESTAMP}"
 
 # 1. Postgres Dump
+echo "Running Postgres backup..."
 bak --debug pg dump \
     --dir "${DIR}" \
     --host "${PGHOST}" \
     --user "${PGUSER}"
 
 # 2. GRIP Backup
+echo "Running GRIP backup..."
 bak --debug grip backup \
     --dir "${DIR}" \
     --host "${GRIP_HOST}" \
@@ -44,6 +46,7 @@ bak --debug grip backup \
     --edge
 
 # 3. S3 Upload
+echo "Running S3 upload..."
 bak --debug s3 upload \
     --dir "${DIR}" \
     --endpoint "${ENDPOINT}" \
@@ -55,6 +58,7 @@ bak --debug s3 upload \
 # We keep the Elasticsearch backups separate from that of Postgres and GRIP
 # to conform to the standard snapshot behavior/structure (e.g. incremental diffs)
 # Ref: https://www.elastic.co/docs/deploy-manage/tools/snapshot-and-restore/self-managed
+echo "Running Elasticsearch backup..."
 bak --debug es dump \
     --endpoint "${ES_ENDPOINT}" \
     --bucket "${ES_BUCKET}" \
